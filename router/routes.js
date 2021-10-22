12827aa5949216a405b59f6b76efdbca19684b33
Parshuram Nikam <parshunikam0612@gmail.com>
2021-10-22 13:07:30 +0530

    cors updated

--- router/routes.js
+++ router/routes.js
@@ -6,6 +6,12 @@ const router = express.Router();
 
 import { addTestimonial, showFile, getAllTestimonials, getTestimonialByID  } from '../controllers/index.controllers.js'
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+next();});
+
 router.get('/', getAllTestimonials);
 
 router.get('/:id', getTestimonialByID);
