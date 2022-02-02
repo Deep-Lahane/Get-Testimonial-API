bfa263ebe9464ba3c40924935d584a3d1191404f
Deep-Lahane <68703328+Deep-Lahane@users.noreply.github.com>
2022-02-02 10:40:00 +0530

    final commit

--- config/db.config.js
+++ config/db.config.js
@@ -3,20 +3,15 @@ import env_vars from "dotenv";
 env_vars.config();
 
 const dbConnection = () => {
-	mongoose
-		.connect(process.env.CONN_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		})
-		.then(() => console.log("Mongoose Connected"))
-		.catch((error) =>
-			console.log("Error in mongoose Connection : \n", error)
-		);
+    mongoose
+        .connect(process.env.CONN_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        .then(() => console.log("Mongoose Connected"))
+        .catch((error) =>
+            console.log("Error in mongoose Connection : \n", error)
+        );
 }
-
-// 'useCreateIndex': true   --> to avoid deprecation warning: 
-// (node:4232) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
-// (Use `node --trace-deprecation ...` to show where the warning was created)
-
 export default dbConnection;
--- controllers/showFile.controller.js
+++ controllers/showFile.controller.js
@@ -3,28 +3,24 @@ import grid from 'gridfs-stream';
 
 let gfs;
 const conn = mongoose.connection;
-conn.once('open', function () {
+conn.once('open', function() {
     gfs = grid(conn.db, mongoose.mongo);
     gfs.collection("uploads");
 })
 
-// readstream sometime not working with updates express version
-//  solution:-  install express 5.13.7 version
-//  command :-  npm i express@5.13.7
 
-const showFile = async(req,res) => {
+const showFile = async(req, res) => {
     try {
         gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
             if (!file) {
                 return res.status(404).json({ status: "failed", message: "No file Found!" });
             }
-            if (err) { 
-                console.log("err >>", err); 
+            if (err) {
+                console.log("err >>", err);
                 return res.status(400).send(err);
             }
-            console.log("file data : ",file);
+            console.log("file data : ", file);
 
-            // install 5.13.11 version to use -> gridfs-stream "createReadStream" function;
             gfs.createReadStream({ filename: file.filename }).pipe(res);
         })
     } catch (error) {
